6df1f715103aa36568be057264135ea37aa01f00
Breniak <kyllian_bocklandt@hotmail.com>
2021-06-29 17:31:51 +0200

    [features camera] setup camera folders

---
+++ Assets/Dev/Kyllian/Camera/Scripts/CameraMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	#region Exposed
+	
+	
+	
+	#endregion
+
+
+	#region Unity API
+	
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+	
+	#endregion
+	
+	
+	#region Utils
+	
+	
+	
+	#endregion
+	
+	
+	#region Private and Protected Members
+	
+	
+	
+	#endregion
+}

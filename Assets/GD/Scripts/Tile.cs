8aad68607d7c9574a7492f0c066de1726f1780ca
Egon <egon.rivera@gmail.com>
2021-06-30 12:35:07 +0200

    redo map with tile prefab

--- Assets/GD/Scripts/Tile.cs
+++ Assets/GD/Scripts/Tile.cs
@@ -4,67 +4,96 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] private Renderer tileRenderer;
+
+    [SerializeField] private Material grassMaterial;
+    [SerializeField] private Material roadWEMaterial;
+    [SerializeField] private Material roadNSMaterial;
+    [SerializeField] private Material roadTurnSWMaterial;
+    [SerializeField] private Material roadTurnSEMaterial;
+    [SerializeField] private Material roadTurnNWMaterial;
+    [SerializeField] private Material roadTurnNEMaterial;
+    [SerializeField] private Material roadCrossMaterial;
 
     [SerializeField] private GameObject constructionZone;
 
-    [SerializeField] private bool constructable = false;
+    //[SerializeField] private bool constructable = false;
 
     public enum TileType
     {
         grass,
-        sand,
         roadWE,
         roadNS,
         roadTurnSW,
         roadTurnSE,
         roadTurnNW,
         roadTurnNE,
-        roadCross
+        roadCross,
+        nonConstructible
     }
 
+    [Header("Parameters")]
     [SerializeField] private TileType tileType = TileType.grass;
 
-    void Start()
+    private void OnValidate()
     {
-        
+        UpdateTile();
     }
 
     private void UpdateTile()
     {
-        constructionZone.SetActive(constructable);
+        //constructionZone.SetActive(constructable);
         Material newMaterial = null;
+
+        bool constructible = false;
 
         switch (tileType)
         {
             case TileType.grass:
-                break;
-            case TileType.sand:
+                constructible = true;
+                if (grassMaterial)
+                    newMaterial = grassMaterial;
                 break;
             case TileType.roadWE:
+                if (roadWEMaterial)
+                    newMaterial = roadWEMaterial;
                 break;
             case TileType.roadNS:
+                if (roadNSMaterial)
+                    newMaterial = roadNSMaterial;
                 break;
             case TileType.roadTurnSW:
+                if (roadTurnSWMaterial)
+                    newMaterial = roadTurnSWMaterial;
                 break;
             case TileType.roadTurnSE:
+                if (roadTurnSEMaterial)
+                    newMaterial = roadTurnSEMaterial;
                 break;
             case TileType.roadTurnNW:
+                if (roadTurnNWMaterial)
+                    newMaterial = roadTurnNWMaterial;
                 break;
             case TileType.roadTurnNE:
+                if (roadTurnNEMaterial)
+                    newMaterial = roadTurnNEMaterial;
                 break;
             case TileType.roadCross:
+                if (roadCrossMaterial)
+                    newMaterial = roadCrossMaterial;
+                break;
+            case TileType.nonConstructible:
+                if (grassMaterial)
+                    newMaterial = grassMaterial;
                 break;
             default:
                 break;
         }
 
-        if(newMaterial)
-            tileRenderer.material = newMaterial;
-    }
+        constructionZone.SetActive(constructible);
 
-    void Update()
-    {
-        
+        if (newMaterial)
+            tileRenderer.material = newMaterial;
     }
 }
